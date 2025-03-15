9376a590bdf83caf6d1f1a8e83624d66ef0f437e
Prajwal Kumar <85426811+prajwalkumar05@users.noreply.github.com>
2025-03-15 15:34:33 +0530

    fixes

--- hackathon/src/components/Layout.jsx
+++ hackathon/src/components/Layout.jsx
@@ -2,11 +2,8 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import TopNavbar from "./TopNavbar";
 import BottomNavbar from "./BottomNavbar";
-<<<<<<< HEAD
 // import BottomNavbar from "./BottomNavBar";
-=======
 
->>>>>>> efe4983a1b0771f7100218d21503471def89ca16
 
 const Layout = () => {
   return (
@@ -15,11 +12,7 @@ const Layout = () => {
       <div className=" pb-20">
         <Outlet /> {/* This will render the current page content */}
       </div>
-<<<<<<< HEAD
       <BottomNavbar/>
-=======
-      <BottomNavbar />
->>>>>>> efe4983a1b0771f7100218d21503471def89ca16
     </div>
   );
 };
